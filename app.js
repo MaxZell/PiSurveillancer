128ebdc4c4b187014f21773b7ef9613c58447303
mzelensky <maxim.zelensky@soxes.ch>
2022-01-05 13:41:12 +0100

    add broke image checker

--- app.js
+++ app.js
@@ -8,16 +8,19 @@ const io = require('socket.io')(server)
 
 const port = process.env.PORT || 8000;
 
+// let server = net.createServer()
+
+
 // server data
-const server_host = '192.168.5.161'
-const server_port = 1234
+const server_host = '192.168.5.81'
+const server_port = 8888
 
 // join frontend path
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
-// serve index.ejs
+// serve views/index.ejs
 app.get('/', (req, res, next)=>{
     res.render('index')
 })
@@ -34,27 +37,32 @@ io.on('connection', (socket)=>{
 		client.connect(server_port, server_host, () => console.log('Connected'))
         // get video from server
         let data = ""
-        let separator = ","
+        const separator = ","
         client.on('data', (chunk) => {
             data += chunk
             if(data.includes(separator)){
-                data = data.slice(0, -separator.length) // remove separator
-                io.emit('frame', `data:image/png;base64,${data}`) // send frame
+                // check png not broken
+                if (data.substring(0,27) === "/9j/4AAQSkZJRgABAQAAAQABAAD") {
+                    data = data.slice(0, -separator.length) // remove separator
+                    io.emit('frame', `data:image/png;base64,${data}`) // send frame to frontend
+                }
                 // reset data
                 chunk = ""
                 data = ""
             }
         })
     })
+    // stop video stream
     socket.on('stop', (msg)=>{
         client.destroy()
     })
 })
 
 // close server connection
-client.on('close', () => {
-    console.log('Connection closed')
-})
+client.on('close', () => console.log('Connection closed'))
+
+// handle errors
+client.on('error', (err) => console.error(err.code))
 
 // start server
 server.listen(port, () => console.log(`Listening on port ${port}`))
