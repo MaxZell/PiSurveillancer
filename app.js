ca4be0837518d18f83ef6c93727ee7d5d3845c1f
mzelensky <maxim.zelensky@soxes.ch>
2022-01-05 15:48:09 +0100

    init commit

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
 const net = require('net')
-const client = new net.Socket()
 const express = require('express')
 const path = require('path')
 const app = express()
-const server = require('http').createServer(app)
-const io = require('socket.io')(server)
+const httpserver = require('http').createServer(app)
+const io = require('socket.io')(httpserver)
 
 const port = process.env.PORT || 8000;
 
-// let server = net.createServer()
-
-
 // server data
-const server_host = '192.168.5.81'
 const server_port = 8888
 
 // join frontend path
@@ -25,44 +20,45 @@ app.get('/', (req, res, next)=>{
     res.render('index')
 })
 
+// tcp server
+const server = net.createServer()
+
 // on user connection
-io.on('connection', (socket)=>{
+io.on('connection', (sock)=>{
     console.log("user connected")
-    socket.on('disconnect', ()=>{
+    sock.on('disconnect', ()=>{
         console.log("user disconnected")
-        client.destroy()
+        // client.destroy()
     })
-    socket.on('start', (msg)=>{
-        // connect to server
-		client.connect(server_port, server_host, () => console.log('Connected'))
-        // get video from server
-        let data = ""
-        const separator = ","
-        client.on('data', (chunk) => {
-            data += chunk
-            if(data.includes(separator)){
-                // check png not broken
-                if (data.substring(0,27) === "/9j/4AAQSkZJRgABAQAAAQABAAD") {
-                    data = data.slice(0, -separator.length) // remove separator
-                    io.emit('frame', `data:image/png;base64,${data}`) // send frame to frontend
+    sock.on('start', (msg)=>{
+        console.log("start")
+        server.on('connection', (socket) => {
+            console.log("got camera connection")
+            let data = ""
+            const separator = ","
+            socket.on('data', (chunk) => {
+                data += chunk
+                if(data.includes(separator)){
+                    // check png not broken
+                    if (data.substring(0,27) === "/9j/4AAQSkZJRgABAQAAAQABAAD") {
+                        data = data.slice(0, -separator.length) // remove separator
+                        io.emit('frame', `data:image/png;base64,${data}`) // send frame to frontend
+                    }
+                    // reset data
+                    chunk = ""
+                    data = ""
                 }
-                // reset data
-                chunk = ""
-                data = ""
-            }
+            })
         })
     })
     // stop video stream
-    socket.on('stop', (msg)=>{
-        client.destroy()
+    sock.on('stop', (msg)=>{
+        // client.destroy()
     })
 })
 
-// close server connection
-client.on('close', () => console.log('Connection closed'))
+// start tcp server
+server.listen(server_port, () => console.log("\x1b[32m", "TCP server started", "\x1b[0m"))
 
-// handle errors
-client.on('error', (err) => console.error(err.code))
-
-// start server
-server.listen(port, () => console.log(`Listening on port ${port}`))
+// start frontend server
+httpserver.listen(port, () => console.log("\x1b[32m", `Frontend listening on port ${port}`, "\x1b[0m"))
