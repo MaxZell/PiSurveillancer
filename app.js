fa5e8c494a27edfeddb30155e1cbdc976802756e
mzelensky <maxim.zelensky@soxes.ch>
2022-01-06 16:47:50 +0100

    fix merge

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const net = require('net')
+const client = new net.Socket()
 const express = require('express')
 const path = require('path')
 const app = express()
-const httpserver = require('http').createServer(app)
-const io = require('socket.io')(httpserver)
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
 
 const port = process.env.PORT || 8000;
 
 // server data
+const server_host = '192.168.5.81'
 const server_port = 8888
 
 // join frontend path
@@ -15,50 +17,44 @@ app.use(express.static(path.join(__dirname, 'frontend')))
 app.set('views', path.join(__dirname, 'frontend'))
 app.set('view engine', 'ejs')
 
-// serve views/index.ejs
+// serve index.ejs
 app.get('/', (req, res, next)=>{
     res.render('index')
 })
 
-// tcp server
-const server = net.createServer()
-
 // on user connection
-io.on('connection', (sock)=>{
-    console.log("user connected")
-    sock.on('disconnect', ()=>{
-        console.log("user disconnected")
-        // client.destroy()
+io.on('connection', (socket)=>{
+    console.log(`user connected: <${socket.handshake.address}>`)
+    socket.on('disconnect', ()=>{
+        console.log(`user disconnected: <${socket.handshake.address}>`)
+        client.destroy()
     })
-    sock.on('start', (msg)=>{
-        console.log("start")
-        server.on('connection', (socket) => {
-            console.log("got camera connection")
-            let data = ""
-            const separator = ","
-            socket.on('data', (chunk) => {
-                data += chunk
-                if(data.includes(separator)){
-                    // check png not broken
-                    if (data.substring(0,27) === "/9j/4AAQSkZJRgABAQAAAQABAAD") {
-                        data = data.slice(0, -separator.length) // remove separator
-                        io.emit('frame', `data:image/png;base64,${data}`) // send frame to frontend
-                    }
-                    // reset data
-                    chunk = ""
-                    data = ""
-                }
-            })
+    socket.on('start', (msg)=>{
+        // connect to server
+		client.connect(server_port, server_host, () => console.log('connected to TCP'))
+        // get video from server
+        let data = ""
+        let separator = ","
+        client.on('data', (chunk) => {
+            data += chunk
+            if(data.includes(separator)){
+                data = data.slice(0, -separator.length) // remove separator
+                io.emit('frame', `data:image/png;base64,${data}`) // send frame
+                // reset data
+                chunk = ""
+                data = ""
+            }
         })
     })
-    // stop video stream
-    sock.on('stop', (msg)=>{
-        // client.destroy()
+    socket.on('stop', (msg)=>{
+        client.destroy()
     })
 })
 
-// start tcp server
-server.listen(server_port, () => console.log("\x1b[32m", "TCP server started", "\x1b[0m"))
+// close server connection
+client.on('close', () => {
+    console.log("\x1b[31m", 'TCP connection closed', "\x1b[0m")
+})
 
 // start frontend server
-httpserver.listen(port, () => console.log("\x1b[32m", `Frontend listening on port ${port}`, "\x1b[0m"))
+server.listen(port, () => console.log("\x1b[32m", `Frontend listening on port ${port}`, "\x1b[0m"))
